d2b254553cba69e7874748466205d702b33d81c7
Clayton Roth <croth0627@yahoo.com>
2017-06-09 00:38:27 -0700

    initial code commit

---
+++ Bogosoft.Hashing.Cryptography.Tests/MockHashableObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bogosoft.Hashing.Cryptography.Tests
+{
+    class MockHashableObject : IHashable
+    {
+        static IEnumerable<byte> GetRandomByteSequence()
+        {
+            var rng = new Random();
+
+            var length = rng.Next(4096);
+
+            var index = 0;
+
+            while(index++ < length)
+            {
+                yield return (byte)rng.Next(0, 255);
+            }
+        }
+
+        byte[] bytes;
+
+        internal MockHashableObject()
+        {
+            bytes = GetRandomByteSequence().ToArray();
+        }
+
+        public byte[] GetHashBytes()
+        {
+            return bytes;
+        }
+    }
+}
---
+++ Bogosoft.Hashing.Cryptography.Tests/UnitTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Should;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Bogosoft.Hashing.Cryptography.Tests
+{
+    [TestFixture, Category("Unit")]
+    public class UnitTests
+    {
+        [TestCase]
+        public void ComputedHashesMatchThoseOfTheirUnderlyingCounterparts()
+        {
+            var hashable = new MockHashableObject();
+
+            var hash = hashable.GetHashBytes();
+
+            var tests = new Dictionary<Type, IHash>
+            {
+                { typeof(MD5CryptoServiceProvider), CryptoHashStrategy.MD5 },
+                { typeof(RIPEMD160Managed), CryptoHashStrategy.RIPEMD160 },
+                { typeof(SHA1CryptoServiceProvider), CryptoHashStrategy.SHA1 },
+                { typeof(SHA256CryptoServiceProvider), CryptoHashStrategy.SHA256 },
+                { typeof(SHA384CryptoServiceProvider), CryptoHashStrategy.SHA384 },
+                { typeof(SHA512CryptoServiceProvider), CryptoHashStrategy.SHA512 }
+            };
+
+            foreach(var x in tests)
+            {
+                using(var algorithm = Activator.CreateInstance(x.Key) as HashAlgorithm)
+                {
+                    x.Value.Compute(hashable).SequenceEqual(algorithm.ComputeHash(hash)).ShouldBeTrue();
+                }
+            }
+        }
+    }
+}
